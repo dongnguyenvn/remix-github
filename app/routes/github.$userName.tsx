ac72ec63eb3092f7baa1e23fff4983ee17463029
Nguyễn Thành Đông <nguyenthanhdong997@gmail.com>
2021-12-25 11:31:24 +0700

    update root

--- app/routes/github.$userName.tsx
+++ app/routes/github.$userName.tsx
@@ -1,21 +1,16 @@
 import { LoaderFunction, useLoaderData } from "remix";
+import { getGithubUser } from "~/features/github/api";
 import { LoaderData, User } from "~/features/github/types";
 
 export const loader : LoaderFunction = async ({params}) => {
-    const res = await fetch(`https://api.github.com/users/${params.userName}`, {
-        headers : {
-            accept : 'application/vnd.github.v3+json',
-            Authorization : 'token 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'
-        }
-    })
-    const {login,avatar_url,html_url,bio} = await res.json()
-    return {
-        user : {login,avatar_url,html_url,bio}
-    }
+    return await getGithubUser(params.userName)
 }
+
+
 
 const Github = () => {
     const {user : {login,avatar_url,html_url,bio}} = useLoaderData<LoaderData>()
+    console.log(avatar_url)
     return (
         <div>
             <p>{login}</p>
